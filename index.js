805a5777e7697d0b4e23301f1b9afca13927f757
Meng Thao <mengthao023@gmail.com>
2022-07-07 19:56:47 -0500

    fourth commit

--- index.js
+++ index.js
@@ -1,23 +1,36 @@
 //Start Game alert//
+//function toggleStart() {
+//window.location.assign("index.html");
 
-window.alert("Player One = Enter Key---Player Two = Space key                                    First player reach to 100 in 30 seconds wins the game!----------------Press [OK] to PLAY!");
+function myStart() {
+    let s = document.getElementById("menu");
+    if (s.style.display === "none") {
+        s.MLDocument.style.display = "block";
+    } else {
+        s.style.display = "none";
+    }
+}
+
+
+//window.alert("Player One = Enter Key---Player Two = Space key                                    First player reach to 100 in 30 seconds wins the game!----------------Press [OK] to PLAY!");
 
 
 //Countdown to 30 Second//
 
 
-
 let timeleft = '30';
-
 
 let downloadTimer = setInterval(function () {
     if (timeleft <= 0) {
         clearInterval(downloadTimer);
         document.getElementById("countDown").innerHTML = "TIMES UP!";
+        window.alert("TRY AGAIN");
     } else {
         document.getElementById("countDown").innerHTML = "CLOCK IS TICKING: " + timeleft + " SECONDS";
+
     }
     timeleft -= 1;
+
 }, 1000);
 
 
@@ -35,14 +48,10 @@ document.addEventListener('keyup', function (e) {
         document.getElementById("clicks").innerHTML = playerOne;
     } if (playerOne >= 5) {
         document.getElementById("clicks").innerHTML = "YOU ARE THE WINNER!";
-        window.alert("PLAYER ONE IS THE WINNER!");
+        window.alert("PLAYER ONE IS THE WINNER! PRESS THE 'RESTART HERE!' TO PLAY AGAIN!");
         clearInterval(downloadTimer);
     }
 })
-
-
-
-
 
 
 
@@ -56,12 +65,12 @@ document.addEventListener('keyup', function (e) {
         document.getElementById("press").innerHTML = playerTwo;
     } if (playerTwo >= 5) {
         document.getElementById("press").innerHTML = "YOU ARE THE WINNER!";
-        window.alert("PLAYER TWO IS THE WINNER!");
+        window.alert("PLAYER TWO IS THE WINNER! PRESS THE 'RESTART HERE!' TO PLAY AGAIN!");
         clearInterval(downloadTimer);
     }
 })
 
-// restart button //
+// Mute function //
 
 
 
